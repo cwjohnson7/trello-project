e8e32e900fa3110772332df67168c1c3451ba705
cwjohnson7 <cliffj1230@gmail.com>
2023-07-14 20:37:45 -0400

    added base url to createThunk

--- server/client/src/features/utilities/createThunk.js
+++ server/client/src/features/utilities/createThunk.js
@@ -6,19 +6,20 @@ const createThunk = (name, apiEndPoint, method) => {
     try {
       let response;
       const config = token ? { headers: { 'Authorization': `Bearer ${token}` } } : {};
+      const base_URL = 'https://trello-jr.onrender.com';
       
       switch (method) {
         case 'GET':
-          response = await axios.get(apiEndPoint, { ...config, params: data });
+          response = await axios.get(base_URL + apiEndPoint, { ...config, params: data });
           break;
         case 'POST':
-          response = await axios.post(apiEndPoint, data, config);
+          response = await axios.post(base_URL + apiEndPoint, data, config);
           break;
         case 'PUT':
-          response = await axios.put(apiEndPoint, data, config);
+          response = await axios.put(base_URL + apiEndPoint, data, config);
           break;
         case 'DELETE':
-          response = await axios.delete(apiEndPoint, data ? { ...config, data } : config);
+          response = await axios.delete(base_URL + apiEndPoint, data ? { ...config, data } : config);
           break;
         default:
           throw new Error('Invalid method');
